d6b7c85b0e4921f4fe1fa5fc3447fbe6c5ebaea6
elias <elias.leonardo.tardon.munoz@everis.com>
2020-07-13 22:20:38 -0400

    feat(validations): add validations for body params and headers
    add validations with yup for body json validation, add params validation for nationalId and add
    validation for content-type

---
+++ validations/validations.js
@@ -0,0 +1,63 @@
+const yup = require('yup');
+
+const validateBody = (validation) => (request, response, next) => {
+    try {
+        validation(request.body);
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+const validateParams = (validation) => (request, response, next) => {
+    try {
+        validation(request.params);
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+const validateHeaders = (validation) => (request, response, next) => {
+    try {
+        validation(request.headers);
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+const validateNationalId = (nationalId) => {
+    const schema = yup.object().shape({
+        nationalId: yup.string('es string')
+            .required()
+            .matches(/([0-9]+-[0-9k]+)/, 'numero'),
+    });
+    schema.validateSync(nationalId);
+};
+const validatePeople = (data) => {
+    const schema = yup.object().shape({
+        nationalId: yup.string('es string')
+            .required()
+            .matches(/([0-9]+-[0-9k]+)/, 'numero'),
+        name: yup.string(),
+        lastName: yup.string(),
+        age: yup.number().integer().positive(),
+        pictureUrl: yup.string().url(),
+    });
+    schema.validateSync(data);
+};
+const validateJsonHeader = (headers) => {
+    const schema = yup.object().shape({
+        'content-type': yup.string().matches('application/json', 'es json'),
+    });
+    schema.validateSync(headers);
+};
+
+module.exports = {
+    validateBody,
+    validateParams,
+    validateHeaders,
+    validateNationalId,
+    validatePeople,
+    validateJsonHeader,
+};
