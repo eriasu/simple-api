359e5b4f8ff1113b8c0e301adfd8b49b40a2643e
elias <elias.leonardo.tardon.munoz@everis.com>
2020-07-16 11:03:20 -0400

    fix(model): update people mode to not allow null on nationalid and remove timestamps

--- src/models/people.js
+++ src/models/people.js
@@ -2,18 +2,26 @@ const People = (sequelize, dataTypes) => sequelize.define('people', {
     nationalId: {
         type: dataTypes.STRING,
         primaryKey: true,
+        allowNull: false,
     },
     name: {
         type: dataTypes.STRING,
+        allowNull: true,
     },
     lastName: {
         type: dataTypes.STRING,
+        allowNull: true,
     },
     age: {
         type: dataTypes.INTEGER,
+        allowNull: true,
     },
     pictureUrl: {
         type: dataTypes.STRING,
+        allowNull: true,
     },
+},
+{
+    timestamps: false,
 });
 exports.people = People;
